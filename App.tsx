a5c96a1de2fdc135b4d8b02eb50bcf75f216a709
Lichi <lisandrobruzzo@gmail.com>
2020-01-05 12:42:26 -0300

    Changed tab space

--- App.tsx
+++ App.tsx
@@ -6,18 +6,18 @@ import store from './src/store/store';
 import AppNavigator from './src/navigation/navigation';
 
 export default function App() {
-  return (
-    <Provider store={store}>
-      <AppNavigator/>
-    </Provider>
-  );
+    return (
+        <Provider store={store}>
+            <AppNavigator/>
+        </Provider>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
 });
