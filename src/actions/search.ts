5dad9c77000094b71f9f4c131866d9e7cde9e098
Lichi <lisandrobruzzo@gmail.com>
2020-01-05 12:40:42 -0300

    Added error handling & changed tab space to 4

--- src/actions/search.ts
+++ src/actions/search.ts
@@ -5,75 +5,81 @@ import store from '../store/store';
 import {CocktailData} from '../components/cocktail/cocktailData';
 
 export interface StartFetch {
-  type: 'START_FETCH',
-  searchText: string
+    type: 'START_FETCH',
+    searchText: string
 };
 
 export interface SuccessFetch {
-  type: 'SUCCESS_FETCH',
-  cocktails: CocktailData[]
+    type: 'SUCCESS_FETCH',
+    cocktails: CocktailData[]
 };
 
 export interface ErrorFetch {
-  type: 'ERROR_FETCH'
+    type: 'ERROR_FETCH'
 };
 
 export interface CancelFetch {
-  type: 'CANCEL_FETCH'
+    type: 'CANCEL_FETCH'
 };
 
 export type ActionTypes = StartFetch | CancelFetch | ErrorFetch | SuccessFetch;
 
 export function successFetchAction(newCocktails: CocktailData[]): ActionTypes {
-  return {
-    type: 'SUCCESS_FETCH',
-    cocktails: newCocktails
-  }
+    return {
+        type: 'SUCCESS_FETCH',
+        cocktails: newCocktails
+    }
 }
 
 export function startFetchAction(searchText: string): ActionTypes {
-  return {
-    type: 'START_FETCH',
-    searchText
-  }
+    return {
+        type: 'START_FETCH',
+        searchText
+    }
 }
 
 export function cancelFetchAction(): ActionTypes {
-  return {
-    type: 'CANCEL_FETCH'
-  }
+    return {
+        type: 'CANCEL_FETCH'
+    }
 }
 
-export const startFetch = (
-  searchText: string//, controller: AbortController
-): ThunkAction<void, RootState, null, Action > => async dispatch => {
-  dispatch(startFetchAction(searchText));
-  if(searchText.length < 4){
-    dispatch(successFetchAction([]));
-    return;
-  }
-  //let success : boolean = false;
-  const response = await fetch(
-    'https://thecocktaildb.com/api/json/v1/1/search.php?s='+searchText, {}
-  )/*.then(
-    (response) => {success=true;}
-  ).catch(function(err) {
-    success=false;
-  });
-  if(!success)
-    return;*/
-  let resData = await response.json();
-  if(store.getState().search.searchText === searchText){
-    let searchResult : CocktailData[];
-    if(resData === undefined)
-      return
-    if(resData.drinks == null){
-      searchResult = [];
-    }else{
-      searchResult = resData.drinks.map((cocktail) =>{
-        return {thumbURL: cocktail.strDrinkThumb,name: cocktail.strDrink,id: cocktail.idDrink}
-      });
+export function errorFetchAction(): ActionTypes {
+    return {
+        type: 'ERROR_FETCH'
     }
-    dispatch(successFetchAction(searchResult));
-  }
+}
+
+export const startFetch = (searchText: string): ThunkAction<void, RootState, null, Action > => async dispatch => {
+    dispatch(startFetchAction(searchText));
+    if(searchText.length < 4){
+        dispatch(successFetchAction([]));
+        return;
+    }
+    await fetch(
+        'https://thecocktaildb.com/api/json/v1/1/search.php?s='+searchText, {}
+    ).then((resp) => {
+        if(resp.ok)
+            return resp.json();
+        else dispatch(errorFetchAction());
+    }).then(
+        (resData) => {
+            console.log('success');
+            if(store.getState().search.searchText === searchText){
+                let searchResult : CocktailData[];
+                if(resData === undefined)
+                    return
+                if(resData.drinks == null){
+                    searchResult = [];
+                }else{
+                    searchResult = resData.drinks.map((cocktail) =>{
+                        return {thumbURL: cocktail.strDrinkThumb,name: cocktail.strDrink,id: cocktail.idDrink}
+                    });
+                }
+                dispatch(successFetchAction(searchResult));
+            }
+        }
+    ).catch(err => {
+        dispatch(errorFetchAction());
+    });
 }
--- src/reducers/search.ts
+++ src/reducers/search.ts
@@ -2,30 +2,32 @@ import { ActionTypes } from '../actions/search'
 import {CocktailData} from '../components/cocktail/cocktailData'
 
 export interface SearchState {
-  cocktails: CocktailData[],
-  isFetching: boolean,
-  searchText: string
+    cocktails: CocktailData[],
+    isFetching: boolean,
+    searchText: string,
+    error: boolean
 }
 
 export const initialState: SearchState = {
-  cocktails: [],
-  isFetching: false,
-  searchText: ''
+    cocktails: [],
+    isFetching: false,
+    searchText: '',
+    error: false
 };
 
 const fetchReducer = (state: SearchState = initialState, action: ActionTypes): SearchState => {
-  switch (action.type) {
-    case 'CANCEL_FETCH':
-      return { cocktails: [], isFetching: false, searchText: ''}
-    case 'START_FETCH':
-      return { cocktails: state.cocktails, isFetching: true,searchText: action.searchText}
-    case 'SUCCESS_FETCH':
-      return { cocktails: action.cocktails, isFetching: false,searchText: state.searchText}
-    case 'ERROR_FETCH':
-      return { cocktails: state.cocktails, isFetching: false,searchText: state.searchText}
-    default:
-      return state;
-  }
+    switch (action.type) {
+        case 'CANCEL_FETCH':
+            return { cocktails: [], isFetching: false, searchText: '', error: false}
+        case 'START_FETCH':
+            return { cocktails: state.cocktails, isFetching: true,searchText: action.searchText, error: state.error}
+        case 'SUCCESS_FETCH':
+            return { cocktails: action.cocktails, isFetching: false,searchText: state.searchText, error: false}
+        case 'ERROR_FETCH':
+            return { cocktails: state.cocktails, isFetching: false,searchText: state.searchText, error: true}
+        default:
+            return state;
+    }
 }
 
 export default fetchReducer;
