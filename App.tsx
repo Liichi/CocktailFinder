839dab6555255adb6d0c7288e0657363749232cb
Lichi <lisandrobruzzo@gmail.com>
2020-01-07 17:49:25 -0300

    Removido estilo sin uso

--- App.tsx
+++ App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import { Provider } from 'react-redux';
 import store from './src/store/store';
 
@@ -12,12 +11,3 @@ export default function App() {
         </Provider>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
-});
